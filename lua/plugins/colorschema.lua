236e8032ef4df3427953a8ae2a790dbcdcb3690f
viz404 <vishal.gxm@gmail.com>
2024-02-13 12:53:45 +0530

    added catppuccin colorscheme as default

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -1,27 +1,43 @@
 return {
-    "rebelot/kanagawa.nvim",
-    config = function()
-        require("kanagawa").setup({
-            commentStyle = { italic = false },
-            keywordStyle = { italic = false },
-            statementStyle = { bold = false },
-            transparent = true,
-            colors = {
-                theme = {
-                    all = {
-                        ui = {
-                            bg_gutter = "none"
+    {
+        "rebelot/kanagawa.nvim",
+        config = function()
+            require("kanagawa").setup({
+                commentStyle = { italic = false },
+                keywordStyle = { italic = false },
+                statementStyle = { bold = false },
+                transparent = true,
+                colors = {
+                    theme = {
+                        all = {
+                            ui = {
+                                bg_gutter = "none"
+                            }
                         }
                     }
-                }
-            },
-            overrides = function(colors)
-                return {
-                    LineNr = { fg = "#878787", bg = colors.theme.ui.bg_gutter },
-                }
-            end,
-        })
-        vim.cmd.colorscheme "kanagawa-dragon"
-    end
+                },
+                overrides = function(colors)
+                    return {
+                        LineNr = { fg = "#878787", bg = colors.theme.ui.bg_gutter },
+                    }
+                end,
+            })
+        end
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+                show_end_of_buffer = true,
+                styles = {
+                    comments = {},
+                    conditionals = {},
+                },
+            })
+            vim.cmd.colorscheme "catppuccin"
+        end
+    },
 }
 
