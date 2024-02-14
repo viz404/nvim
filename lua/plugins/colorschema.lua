435d8525904a4ca864458fae6aba5549c2df63b8
viz404 <vishal.gxm@gmail.com>
2024-02-14 11:06:07 +0530

    changed colorscheme and statusline options

--- lua/plugins/colorschema.lua
+++ lua/plugins/colorschema.lua
@@ -35,9 +35,23 @@ return {
                     comments = {},
                     conditionals = {},
                 },
+                highlight_overrides = {
+                    mocha = function(mocha)
+                        return {
+                            LineNr = { fg = mocha.overlay0 },
+                        }
+                    end,
+                },
+                color_overrides = {
+                    mocha = {
+                        text = "#d8def2",
+                        base = "#000000",
+                        mantle = "#000000",
+                        crust = "#000000",
+                    },
+                },
             })
             vim.cmd.colorscheme "catppuccin"
         end
     },
 }
-
